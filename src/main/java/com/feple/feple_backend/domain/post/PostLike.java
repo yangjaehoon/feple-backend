fa917e08c1dcae48fdb36100d5cd2e621c423368
Jaehoon Yang <lljhms@naver.com>
2025-03-25 22:04:47 +0900

    좋아요 기능 엔티티

---
+++ src/main/java/com/feple/feple_backend/domain/post/PostLike.java
@@ -0,0 +1,28 @@
+package com.feple.feple_backend.domain.post;
+
+import com.feple.feple_backend.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"user_id", "post_id"})
+        }
+)
+
+public class PostLike {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+}
