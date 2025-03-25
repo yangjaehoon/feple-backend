9738d254082d0d0c21dd6270ee56d23153f2acd2
Jaehoon Yang <lljhms@naver.com>
2025-03-25 21:54:03 +0900

    댓글 엔티티 생성

---
+++ src/main/java/com/feple/feple_backend/domain/post/Comment.java
@@ -0,0 +1,31 @@
+package com.feple.feple_backend.domain.post;
+
+import com.feple.feple_backend.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+}
