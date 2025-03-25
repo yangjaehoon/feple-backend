336abb2932b8cf80e8bb0de3ab05ebccf21ceb83
Jaehoon Yang <lljhms@naver.com>
2025-03-25 15:57:03 +0900

    git init

---
+++ src/main/java/com/feple/feple_backend/FepleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.feple.feple_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FepleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FepleBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feple/feple_backend/FepleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feple.feple_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FepleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
