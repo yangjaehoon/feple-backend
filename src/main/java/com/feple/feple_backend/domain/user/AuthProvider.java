1d29598ba8014b9a5b0951ce05470ca5c5eb025f
Jaehoon Yang <lljhms@naver.com>
2025-03-25 20:25:49 +0900

    user doamin 생성

---
+++ src/main/java/com/feple/feple_backend/domain/user/AuthProvider.java
@@ -0,0 +1,5 @@
+package com.feple.feple_backend.domain.user;
+
+public enum AuthProvider {
+    KAKAO, NAVER, GOOGLE
+}
---
+++ src/main/java/com/feple/feple_backend/domain/user/User.java
@@ -0,0 +1,31 @@
+package com.feple.feple_backend.domain.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.security.AuthProvider;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickname;
+
+    @Column(unique = true, nullable = false)
+    private String oauthId;
+
+    @Enumerated(EnumType.STRING)
+    private AuthProvider provider;
+
+    private String profileImageUrl;
+
+    private String email;
+
+}
