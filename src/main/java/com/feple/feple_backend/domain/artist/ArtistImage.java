7abe2ad1ff11cc05a874760f140457a0ac7a96b9
Jaehoon Yang <lljhms@naver.com>
2025-03-27 15:18:53 +0900

    아티스트 이미지 직접 추가

---
+++ src/main/java/com/feple/feple_backend/domain/artist/ArtistImage.java
@@ -0,0 +1,39 @@
+package com.feple.feple_backend.domain.artist;
+
+import com.feple.feple_backend.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class ArtistImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "artist_id")
+    private Artist artist;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "uploader_id")
+    private User uploader;
+
+    private LocalDateTime uploadAt;
+
+    @OneToMany(mappedBy = "artistImage", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ArtistImageLike> likes = new ArrayList<>();
+
+    public int getLikeCount(){
+        return likes.size();
+    }
+}
