065482a75052711345ef9e3802a359ed260c5d28
Jaehoon Yang <lljhms@naver.com>
2025-04-02 12:47:30 +0900

    s3서비스 생성

---
+++ src/main/java/com/feple/feple_backend/service/s3/S3Service.java
@@ -0,0 +1,58 @@
+package com.feple.feple_backend.service.s3;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Service
+public class S3Service {
+
+    private final S3Client s3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    public S3Service(@Value("${cloud.aws.credentials.access-key}") String accessKey,
+                     @Value("${cloud.aws.credentials.secret-key}") String secretKey,
+                     @Value("${cloud.aws.region.static}") String region) {
+
+        this.s3Client = S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)))
+                .build();
+    }
+
+    public String upload(MultipartFile file, String folder) throws IOException {
+        String filename = createFileName(file.getOriginalFilename(), folder);
+
+        PutObjectRequest request = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(filename)
+                .acl(ObjectCannedACL.PUBLIC_READ)
+                .build();
+
+        s3Client.putObject(request, software.amazon.awssdk.core.sync.RequestBody.fromInputStream(
+                file.getInputStream(), file.getSize()));
+
+        return "https://" + bucketName + ".s3.amazonaws.com/" + filename;
+    }
+
+    private String createFileName(String originalName, String folder) {
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+        String encodeName = URLEncoder.encode(originalName, StandardCharsets.UTF_8);
+        return folder + "/" + timestamp + "_" + encodeName;
+    }
+}
