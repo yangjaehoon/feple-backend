75e131ac8e91ba7f1ff080f27b3873bd7aa1fb47
Jaehoon Yang <lljhms@naver.com>
2026-01-03 22:47:05 +0900

    Lombok 어노테이션 추가

--- src/main/java/com/feple/feple_backend/festival/dto/FestivalRequestDto.java
+++ src/main/java/com/feple/feple_backend/festival/dto/FestivalRequestDto.java
@@ -1,12 +1,18 @@
 package com.feple.feple_backend.festival.dto;
 
 import com.feple.feple_backend.artist.dto.ArtistRequestDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class FestivalRequestDto {
     private String title;
     private String description;
--- src/main/java/com/feple/feple_backend/festival/dto/FestivalResponseDto.java
+++ src/main/java/com/feple/feple_backend/festival/dto/FestivalResponseDto.java
@@ -1,13 +1,18 @@
 package com.feple.feple_backend.festival.dto;
 
 import com.feple.feple_backend.artist.dto.ArtistResponseDto;
+import com.feple.feple_backend.festival.domain.Festival;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class FestivalResponseDto {
     private Long id;
@@ -18,5 +23,17 @@ public class FestivalResponseDto {
     private LocalDate endDate;
     private String posterUrl;
     private List<ArtistResponseDto> artists;
+
+    public static FestivalResponseDto from(Festival festival) {
+        return FestivalResponseDto.builder()
+                .id(festival.getId())
+                .title(festival.getTitle())
+                .description(festival.getDescription())
+                .location(festival.getLocation())
+                .startDate(festival.getStartDate())
+                .endDate(festival.getEndDate())
+                .posterUrl(festival.getPosterUrl())
+                .build();
+    }
 
 }
