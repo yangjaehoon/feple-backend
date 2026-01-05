d19954ba3789e197ad1d1213e0ed2fc0b0b5e0f1
Jaehoon Yang <lljhms@naver.com>
2026-01-05 18:08:30 +0900

    페스티벌 목록 dto 수정

---
+++ src/main/java/com/feple/feple_backend/festival/dto/FestivalResponseDto.java
@@ -0,0 +1,36 @@
+package com.feple.feple_backend.festival.dto;
+
+import com.feple.feple_backend.festival.domain.Festival;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FestivalResponseDto {
+
+    private Long id;
+    private String title;
+    private String description;
+    private String location;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String posterUrl;
+
+    public static FestivalResponseDto from(Festival festival) {
+        return FestivalResponseDto.builder()
+                .id(festival.getId())
+                .title(festival.getTitle())
+                .description(festival.getDescription())
+                .location(festival.getLocation())
+                .startDate(festival.getStartDate())
+                .endDate(festival.getEndDate())
+                .posterUrl(festival.getPosterUrl())
+                .build();
+    }
+}
