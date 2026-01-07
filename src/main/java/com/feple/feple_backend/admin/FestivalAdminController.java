ce634c579118c3dc532723c56223243128532370
Jaehoon Yang <lljhms@naver.com>
2026-01-07 17:17:06 +0900

    관리자용 페스티벌 등록 페이지 controller

--- src/main/java/com/feple/feple_backend/admin/FestivalAdminController.java
+++ src/main/java/com/feple/feple_backend/admin/FestivalAdminController.java
@@ -1,4 +1,33 @@
 package com.feple.feple_backend.admin;
 
+import com.feple.feple_backend.festival.dto.FestivalRequestDto;
+import com.feple.feple_backend.festival.dto.FestivalResponseDto;
+import com.feple.feple_backend.festival.service.FestivalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin/festivals")
 public class FestivalAdminController {
+
+    private final FestivalService festivalService;
+
+    @GetMapping("/new")
+    public String showCreateForm(Model model) {
+        model.addAttribute("festival", new FestivalRequestDto());
+        return "admin/festival-form";
+    }
+
+    @PostMapping("/new")
+    public String createFestival(FestivalRequestDto dto){
+        festivalService.createFestival(dto);
+        return "redirect:/admin/festivals/new?success";
+    }
+
+
 }
