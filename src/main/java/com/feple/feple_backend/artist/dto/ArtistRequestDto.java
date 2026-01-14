ba2666ea638870d02429697885c8dc356d04b18f
Jaehoon Yang <lljhms@naver.com>
2026-01-14 22:23:17 +0900

    setter 추가

--- src/main/java/com/feple/feple_backend/artist/dto/ArtistRequestDto.java
+++ src/main/java/com/feple/feple_backend/artist/dto/ArtistRequestDto.java
@@ -1,8 +1,10 @@
 package com.feple.feple_backend.artist.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class ArtistRequestDto {
     private String name;
     private String genre;
