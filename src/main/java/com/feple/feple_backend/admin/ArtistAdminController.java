0c2567e6f181fd81fd2a0fc1c0f488c53bf563ff
Jaehoon Yang <lljhms@naver.com>
2026-01-15 16:13:12 +0900

    아티스트 등록 컨트롤러 생성

---
+++ src/main/java/com/feple/feple_backend/admin/ArtistAdminController.java
@@ -0,0 +1,38 @@
+package com.feple.feple_backend.admin;
+
+import com.feple.feple_backend.artist.domain.Artist;
+import com.feple.feple_backend.artist.dto.ArtistRequestDto;
+import com.feple.feple_backend.service.ArtistService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/artists")
+@RequiredArgsConstructor
+public class ArtistAdminController {
+
+    private final ArtistService artistService;
+
+    @GetMapping("/new")
+    public String showCreateForm(Model model) {
+        model.addAttribute("artist", new ArtistRequestDto());
+        return "admin/artists-form";
+    }
+
+    @PostMapping("/new")
+    public String createArtist(@ModelAttribute("artist") ArtistRequestDto dto) {
+        artistService.createArtist(dto);
+        return "redirect:/admin/artists";
+    }
+
+    @GetMapping
+    public String listArtists(Model model) {
+        model.addAttribute("artists", artistService.getAllArtists());
+        return "admin/artists-list";
+    }
+}
