d46650d9097639d25263a9a9e65e34142c5a17b8
Jaehoon Yang <lljhms@naver.com>
2026-01-18 22:03:19 +0900

    artistfestival reopository 생성

---
+++ src/main/java/com/feple/feple_backend/artistfestival/repository/ArtistFestivalRepository.java
@@ -0,0 +1,13 @@
+package com.feple.feple_backend.artistfestival.repository;
+
+import com.feple.feple_backend.artistfestival.domain.ArtistFestival;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ArtistFestivalRepository extends JpaRepository<ArtistFestival, Long> {
+
+    List<ArtistFestival> findByFestivalId(Long festivalId);
+
+    boolean existsByFestivalIdAndArtistId(Long festivalId, Long artistId);
+}
