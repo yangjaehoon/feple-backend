e5e11da5017579705b71ba120423a8ce37264baa
Jaehoon Yang <lljhms@naver.com>
2026-02-06 23:16:42 +0900

    아티스트 페스티벌에 setter추가

--- src/main/java/com/feple/feple_backend/artistfestival/dto/ArtistFestivalCreateRequest.java
+++ src/main/java/com/feple/feple_backend/artistfestival/dto/ArtistFestivalCreateRequest.java
@@ -3,8 +3,10 @@ package com.feple.feple_backend.artistfestival.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class ArtistFestivalCreateRequest {
 
