8674cad27d85da0e6637647d7511adafe5777951
Jaehoon Yang <lljhms@naver.com>
2026-02-12 19:42:41 +0900

    provider와 oauthid 같이 조회

--- src/main/java/com/feple/feple_backend/user/repository/UserRepository.java
+++ src/main/java/com/feple/feple_backend/user/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.feple.feple_backend.user.repository;
 import com.feple.feple_backend.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.security.AuthProvider;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -10,4 +11,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     Optional<User> findByOauthId(String oauthId);
+
+    Optional<User> findByProviderAndOauthId(AuthProvider provider, String oauthId);
+
 }
