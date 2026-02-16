043bfefe08c544b0884676a0fe051514ff31f75b
Jaehoon Yang <lljhms@naver.com>
2026-02-16 18:04:57 +0900

    s3  버킷 이름 수정

--- src/main/java/com/feple/feple_backend/config/S3Config.java
+++ src/main/java/com/feple/feple_backend/config/S3Config.java
@@ -2,6 +2,7 @@ package com.feple.feple_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -12,7 +13,7 @@ public class S3Config {
     public S3Presigner s3Presigner() {
         return S3Presigner.builder()
                 .region(Region.AP_NORTHEAST_2)
-                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
